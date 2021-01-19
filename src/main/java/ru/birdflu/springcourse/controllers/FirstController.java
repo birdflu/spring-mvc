a9b61418739fbc7d7f7ddee7b82c3048af78e64b
birdflu <birdflu@mail.ru>
2021-01-19 23:01:23 +0300

    Lesson 20

--- src/main/java/ru/birdflu/springcourse/controllers/FirstController.java
+++ src/main/java/ru/birdflu/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.birdflu.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
   @GetMapping("/hello")
-  public String helloPage(HttpServletRequest request) {
-    String name = request.getParameter("name");
-    String surname = request.getParameter("surname");
-//  public String helloPage(@RequestParam(value = "name", required = false) String name,
-//                          @RequestParam(value = "surname", required = false) String surname) {
-    System.out.println("Hello, " + name + " " + surname);
+//  public String helloPage(HttpServletRequest request) {
+//    String name = request.getParameter("name");
+//    String surname = request.getParameter("surname");
+  public String helloPage(@RequestParam(value = "name", required = false) String name,
+                          @RequestParam(value = "surname", required = false) String surname,
+                          Model model) {
+    //System.out.println("Hello, " + name + " " + surname);
+    model.addAttribute("message", "Hello, " + name + " " + surname);
     return "first/hello";
   }
 
