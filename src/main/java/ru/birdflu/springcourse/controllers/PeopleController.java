26ad861ffd98ae8d29c4ce6f0ffa5a7de633868a
birdflu <birdflu@mail.ru>
2021-01-20 14:06:53 +0300

    Lesson 22

--- src/main/java/ru/birdflu/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/birdflu/springcourse/controllers/PeopleController.java
@@ -2,10 +2,9 @@ package ru.birdflu.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.birdflu.springcourse.dao.PersonDAO;
+import ru.birdflu.springcourse.model.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,4 +30,21 @@ public class PeopleController {
     return "people/show";
    }
 
+//   @GetMapping("/new")
+//  public String newPerson(Model model) {
+//     model.addAttribute("person", new Person());
+//    return "people/new";
+//  }
+
+  @GetMapping("/new")
+  public String newPerson(@ModelAttribute("person") Person person) {
+    return "people/new";
+  }
+
+
+  @PostMapping()
+  public String create(@ModelAttribute("person") Person person) {
+    personDAO.save(person);
+    return "redirect:/people";
+  }
 }
